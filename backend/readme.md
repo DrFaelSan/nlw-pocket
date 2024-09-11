d1ad07292f55607e4bca4337699b53872117208b
DrFaelSan <rafaelvplima@gmail.com>
2024-09-11 11:48:57 -0300

    conclusão da aula 1

---
+++ backend/readme.md
@@ -0,0 +1,11 @@
+#figma:
+https://www.figma.com/design/KUjfYNb4n2E16zCGDMt2wK/NLW-Pocket-JS-%E2%80%A2-in.orbit-(Community)?node-id=82-2&node-type=canvas&t=xS2f65V2GN57EB53-0
+
+#gerar scripts do schema
+    - npx drizzle-kit generate
+
+#executar migration
+    - npx drizzle-kit migrate
+
+#ver banco de dados 
+    - npx drizzle-kit studio
---
+++ backend/src/functions/get-week-pending-goals.ts
@@ -0,0 +1,51 @@
+import dayjs from 'dayjs'
+import { db } from '../db'
+import { goalCompletions, goals } from '../db/schema'
+import { and, gte, lte, count, eq, sql } from 'drizzle-orm'
+
+export const firtDayOfWeek = dayjs().startOf('week').toDate()
+export const lastDayOfWeek = dayjs().endOf('week').toDate()
+
+export async function getWeekPendingGoals() {
+  const goalsCreateUpToWeek = db.$with('goals_created_up_to_week').as(
+    db
+      .select({
+        id: goals.id,
+        title: goals.title,
+        desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+        createdAt: goals.createdAt,
+      })
+      .from(goals)
+      .where(lte(goals.createdAt, lastDayOfWeek))
+  )
+
+  const goalCompletionCounts = db.$with('goal_completion_counts').as(
+    db
+      .select({
+        goalId: goalCompletions.goalId,
+        completionCount: count(goalCompletions.id).mapWith(Number).as('completionCount'),
+      })
+      .from(goalCompletions)
+      .where(and(gte(goalCompletions.createdAt, firtDayOfWeek), lte(goalCompletions.createdAt, lastDayOfWeek)))
+      .groupBy(goalCompletions.goalId)
+  )
+
+  const pendingGoals = await db
+    .with(goalsCreateUpToWeek, goalCompletionCounts)
+    .select({
+      id: goalsCreateUpToWeek.id,
+      title: goalsCreateUpToWeek.title,
+      desiredWeeklyFrequency: goalsCreateUpToWeek.desiredWeeklyFrequency,
+      completionCount: sql /*sql*/`
+        COALESCE(${goalCompletionCounts.completionCount},0)
+      `
+        .mapWith(Number)
+        .as('completionCount'),
+    })
+    .from(goalsCreateUpToWeek)
+    .leftJoin(goalCompletionCounts, eq(goalCompletionCounts.goalId, goalsCreateUpToWeek.id))
+
+  return {
+    pendingGoals,
+  }
+}
